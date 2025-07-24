74710fd9a382f7429a1000b931217478737d1db1
tebteban <tebancejas@gmail.com>
2025-07-24 17:34:06 -0300

    sprint 2

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
 const express = require('express');
-const app = express();
-const path = require('path');
+const path = require('path'); // Módulo para manejar rutas de archivos
 
-// Configurar carpeta pública para CSS, imágenes, etc.
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// Esto le dice a Express que sirva los archivos estáticos desde la carpeta 'public'.
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Ruta principal
+// Ruta para servir index.html (tu página principal)
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-// Levantar servidor
-const PORT = 3000;
+// Ruta para servir prod.html
+app.get('/productos', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'productos.html'));
+});
+
+// Ruta para servir sign_in.html(inicio de sesion)
+app.get('/sign_in', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'sign_in.html'));
+});
+
+// Ruta para servir register.html(registro de usuario)
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'register.html'));
+});
+
+// Ruta para servir cart.html(carrito de compras)
+app.get('/cart', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'cart.html'));
+});
+
+// Inicia el servidor
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
